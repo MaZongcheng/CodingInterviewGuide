a7c14965c0d797bcbeb181a31e4f6b5bf7ca5983
unknown <>
2017-09-13 19:54:46 +0800

    vscode commit test

---
+++ src/SchoolRecruit/jingritoutiao2018/Problem_03.java
@@ -0,0 +1,10 @@
+package SchoolRecruit.jingritoutiao2018;
+
+/********************************************************************************
+ * CodingInterviewGuide -- 
+ * @version 2017/09/12 22:09
+ * @author Î÷ÌÆÍõ, xtwyzh@gmail.com,xtwroot.com
+ * xtwroot Copyrights (c) 2017. All rights reserved.
+ ********************************************************************************/
+public class Problem_03 {
+}
---
+++ src/SchoolRecruit/jingritoutiao2018/problem_02.java
@@ -0,0 +1,10 @@
+package SchoolRecruit.jingritoutiao2018;
+
+/********************************************************************************
+ * CodingInterviewGuide -- 
+ * @version 2017/09/09 19:15
+ * @author Î÷ÌÆÍõ, xtwyzh@gmail.com,xtwroot.com
+ * xtwroot Copyrights (c) 2017. All rights reserved.
+ ********************************************************************************/
+public class problem_02 {
+}
